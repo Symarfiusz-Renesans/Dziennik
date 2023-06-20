ff2b0f4d6494e0d15201f4e546f7e6c61c27c696
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2023-06-20 20:43:39 +0200

    utworzenie działającego systemu logowania

--- Strona-Dziennik/Podstrony/logowanie.php
+++ Strona-Dziennik/Podstrony/logowanie.php
@@ -4,38 +4,28 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Logowanie</title>
-	<link rel="stylesheet" href="style/stylDoLogowania.css">
+	<link rel="stylesheet" href="../style/stylDoLogowania.css">
 </head>
 <body>
-
-
-	
-<?php
-
-		include 'czystyPHP/check.php';
-
-
-   	if(!isset($_COOKIE['typUzytkownika'])){
-
-   		setcookie("typUzytkownika", null, -1, "/");
-   		setcookie("uzytkownik", null, -1, "/");
-		setcookie("wylogowany", null, -1, "/");
-		header("Location: Podstrony/logowanie.php");
-
-    }
-
-?>
 
 <main>
 	<header class="flexBox">
 		<h1>Dziennik</h1>
 	</header>
 	<div class="flexBox">
+
 		<div id="loguj">
 			<header class="flexBox">
 				<h1>Zaloguj się!</h1>
 			</header>
-			<form action="" class="flexBoxForm flexBox">
+			<form action="../czystyPHP/sprawdzHaslo.php" method="POST" class="flexBoxForm flexBox">
+
+
+				<?php
+					if(isset($_COOKIE['error'])){
+						echo $_COOKIE["error"];
+					}
+				?>
 				<label>Login:</label><input type="text" name="login"><br>
 				<label>Hasło:</label><input type="password" name="haslo"><br>
 				<button>Kontynuuj</button>
