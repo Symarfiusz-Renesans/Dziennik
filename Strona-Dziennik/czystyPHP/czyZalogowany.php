93a2e0400a534b8dac7a1b03c1ddc81e08b62197
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2023-06-22 15:36:41 +0200

    szybka naprawa skryptu sprawdzającego czy jest się zalogowanym

--- Strona-Dziennik/czystyPHP/czyZalogowany.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(!isset($_COOKIE['typUzytkownika'])){
-	setcookie("typUzytkownika", null, -1, "/");
- 	setcookie("uzytkownik", null, -1, "/");
-	header("Location: ../Podstrony/logowanie.php");
-}
-
-?>
