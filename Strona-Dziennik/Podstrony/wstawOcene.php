314b2233294ae60bd3763334197622d23ee189fa
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2023-07-04 10:01:36 +0200

    dodanie funkcji wpisywania ocen

--- Strona-Dziennik/Podstrony/wstawOcene.php
+++ Strona-Dziennik/Podstrony/wstawOcene.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Wstaw ocene</title>
-	<link rel="stylesheet" href="../style/stylDoLogowania.css">
+	<link rel="stylesheet" href="../style/stylDoFormularzy.css">
 </head>
 <body>
 	
@@ -13,33 +13,34 @@
 		<header class="flexBox">
 			<h1>Twoje Praktyki!</h1>
 		</header>
-		<div>
 
-	<?php
-		include '../czystyPHP/check.php';
-		include '../czystyPHP/czyZalogowany.php';
+			<?php
+				if (isset($_COOKIE['error'])) {
+					echo $_COOKIE['error'];
+				}
+				include '../czystyPHP/check.php';
+				include '../czystyPHP/czyZalogowany.php';
 
-		$wybierzUzytkownika = 'SELECT * FROM dbo.wyswietlWszystkichStudentow() WHERE idSzkoly = '.$_GET['szkola'];
-		$result = sqlsrv_query($polaczenie ,$wybierzUzytkownika);
-		echo '<form action="czystyPHP/WDBocene.php" method="POST" class="flexBoxForm flexBox"><label>Uczeń</label><select name="uczen"> ';
-		while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
-			echo '<option value="'.$row['Indeks'].'">';
+				$wybierzUzytkownika = 'SELECT * FROM dbo.wyswietlWszystkichStudentow() WHERE idSzkoly = '.$_GET['szkola'];
+				$result = sqlsrv_query($polaczenie ,$wybierzUzytkownika);
+				echo '<form action="../czystyPHP/WDBocene.php?szkola='.$_GET['szkola'].'" method="POST" class="flexBoxForm flexBox"><label>Uczeń</label><select name="uczen"> ';
+				while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+					echo '<option value="'.$row['Indeks'].'">';
 			
-		    echo $row['imie'].' '.$row['nazwisko'].'</option>';
-		}
-		echo '</select><label>Ocena</label><input type="number" name="ocena" min=0 max=6><label>Przedmiot</label><select name="przedmiot">';
+			    	echo $row['imie'].' '.$row['nazwisko'].'</option>';
+				}
+				echo '</select><label>Ocena</label><input type="number" name="ocena" min=0 max=6><label>Przedmiot</label><select name="przedmiot">';
 
-		$funWWP = "SELECT * FROM Przedmioty";
-		$resultWWP = sqlsrv_query($polaczenie, $funWWP);
+				$funWWP = "SELECT * FROM Przedmioty";
+				$resultWWP = sqlsrv_query($polaczenie, $funWWP);
 
-		while($row = sqlsrv_fetch_array( $resultWWP, SQLSRV_FETCH_ASSOC)){
-			echo'<option value='.$row['idPrzedmiotu'].'>'.$row['Przedmiot'].'</option>';
-		}
-	?>
+				while($row = sqlsrv_fetch_array( $resultWWP, SQLSRV_FETCH_ASSOC)){
+					echo'<option value='.$row['idPrzedmiotu'].'>'.$row['Przedmiot'].'</option>';
+				}
+			?>
 
-		</select><br><br><button>Potwierdź</button></form>
-
-		</div>
+			</select><br><br><button>Potwierdź</button>
+		</form>
 	</div>
 </main>
 
--- Strona-Dziennik/czystyPHP/WDBocene.php
+++ Strona-Dziennik/czystyPHP/WDBocene.php
@@ -4,8 +4,19 @@
 
 	$query = 'EXEC dodajOcene @indeks = '.$_POST['uczen'].', @przedmiot = '.$_POST['przedmiot'].', @ocena = '.$_POST['ocena'];
 
-	sqlsrv_query($polaczenie, $query);
+	if (sqlsrv_query($polaczenie, $query) === false) {
+		$error = sqlsrv_errors();
+		foreach( $error as $e ) {
+			setcookie("error", $e["message"], time()+60, "/");
+        }
+        echo $_COOKIE['error'];
 
-	header("Location:..dziennik.php");
+        
+	} else {
+		setcookie("error", "Pomyślnie dodano ocenę!", time()+60, "/");
+	}
+
+	header("Location:../Podstrony/wstawOcene.php?szkola=".$_GET['szkola']."");
+
 
 ?>
